45370c2643445de16dbc46c492b231babe9fcedd
AtomsForPeace <adam.p.bannister@gmail.com>
2016-03-21 21:18:29 +0100

    basic attempt. need to work on NaN values

---
+++ test/clojure_bcubed/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-bcubed.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-bcubed.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

ce4a4d9b59bcb688caf352668d64cb7478b53b1b
Adam Bannister <adam@silurian.zblmath.fiz-karlsruhe.de>
2016-10-26 14:28:39 +0200

    add test for average and empty tests for other functions

--- test/clojure_bcubed/core_test.clj
+++ test/clojure_bcubed/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [clojure-bcubed.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-average
+  (is (= 2 (average [2 2])))
+  (is (= 5/2 (average [1 2 3 4])))
+)
+
+(deftest test-multi-recall)
+
+(deftest test-recall)
+
+(deftest test-recall-sensible)
+
+(deftest test-multi-precision)
+
+(deftest test-precision)
+
+(deftest test-precision-sensible)

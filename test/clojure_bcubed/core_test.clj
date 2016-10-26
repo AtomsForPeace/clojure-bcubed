f1fb40f050bfba41031838647676253f45ec56e9
Adam Bannister <adam@silurian.zblmath.fiz-karlsruhe.de>
2016-10-26 14:55:40 +0200

    add tests for recall, precision, recall sensible, precision sensible

--- test/clojure_bcubed/core_test.clj
+++ test/clojure_bcubed/core_test.clj
@@ -2,19 +2,52 @@
   (:require [clojure.test :refer :all]
             [clojure-bcubed.core :refer :all]))
 
+(def categories {
+  :item1 #{"black"}
+  :item2 #{"gray"}
+  :item3 #{"gray"}
+  :item4 #{"black"}
+  :item5 #{"black"}
+  :item6 #{"dashed"}
+  :item7 #{"dashed"}
+  }
+)
+(def clusters {
+  :item1 #{"A" "B"}
+  :item2 #{"A" "B"}
+  :item3 #{"A"}
+  :item4 #{"B"}
+  :item5 #{"B"}
+  :item6 #{"C"}
+  :item7 #{"C"}
+  }
+)
+
 (deftest test-average
   (is (= 2 (average [2 2])))
   (is (= 5/2 (average [1 2 3 4])))
 )
 
-(deftest test-multi-recall)
+(deftest test-multi-recall
+  
+)
 
-(deftest test-recall)
+(deftest test-recall
+  (is (= 1.0 (recall categories clusters)))
+)
 
-(deftest test-recall-sensible)
+(deftest test-recall-sensible
+  (is (= 1.0 (recall-sensible categories clusters)))
+)
 
-(deftest test-multi-precision)
+(deftest test-multi-precision
+  
+)
 
-(deftest test-precision)
+(deftest test-precision
+  (is (= 0.64 (precision categories clusters)))
+)
 
-(deftest test-precision-sensible)
+(deftest test-precision-sensible
+  (is (= 0.5555555555555556 (precision-sensible categories clusters)))
+)
